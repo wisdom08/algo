4be9782087f3a81d8fc299ab81e48aa15b286734
wisdom08 <wisdom08@kakao.com>
2025-03-08 16:52:21 +0900

    Add note for findWordsContaining

--- 2942-find-words-containing-character/2942-find-words-containing-character.py
+++ 2942-find-words-containing-character/2942-find-words-containing-character.py
@@ -1,3 +1,4 @@
+# 회고: for 변수 in 리스트를 이용해서 원소 존재 여부를 확인하고 이중 for문이 아닌 하나의 for문으로 해결할 수 있었음
 class Solution:
     def findWordsContaining(self, words: List[str], x: str) -> List[int]:
         res = []
