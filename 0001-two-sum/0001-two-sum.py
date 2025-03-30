3314b03091f7c75e3103d1c9172b6975e5b12fe9
wisdom08 <wisdom08@kakao.com>
2025-03-30 18:56:17 +0900

    Add note for twoSum

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/two-sum/
+# 회고: 해시테이블을 이용해서 시간복잡도를 개선할 수 있음
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)-1):
