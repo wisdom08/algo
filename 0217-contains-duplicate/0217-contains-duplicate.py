8aac94c672f28d85c53b3f893056f1501c7cc317
wisdom08 <wisdom08@kakao.com>
2025-03-30 17:04:57 +0900

    Time: 9 ms (63.88%), Space: 31.6 MB (29.78%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        set_nums = set(nums)
+        if len(nums) == len(set_nums):
+            return False
+        else:
+            return True
+
+        
