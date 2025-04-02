75acd3c62e23498537401e402fc42695c1fdcafc
wisdom08 <wisdom08@kakao.com>
2025-04-02 23:58:05 +0900

    Add note for longestCommonPrefix

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,19 +1,21 @@
+# https://leetcode.com/problems/longest-common-prefix/
+# 회고: all() 함수, list.append() 후 ''.join(list)
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         answer = ''
         strs.sort()
         for i in range(len(strs[0])):
             curr = strs[0][i]
-            if self.includesCharacter(strs,curr,i):
+            if self.includesCharacter(strs, curr, i):
                 answer += curr
             else:
                 break
         return answer
-            
+
     def includesCharacter(self, strs, curr, i):
         for x in strs[1:]:
             if x[i] == curr:
                 continue
             else:
-            	return False
+                return False
         return True
