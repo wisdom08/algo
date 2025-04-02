f6b7d4b16b0e6e87e5a620187b8b2076696282b5
wisdom08 <wisdom08@kakao.com>
2025-04-02 23:59:32 +0900

    Add note for removeElement

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,3 +1,5 @@
+# https://leetcode.com/problems/remove-element/
+# 회고: 리스트 길이만큼 맨 앞의 요소를 검사해서 val이면 제거하고, 아니면 뒤로 보냄
 class Solution:
     def removeElement(self, nums, val):
         for i in range(len(nums)):
