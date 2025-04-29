4a1e8315ef6a6769710234839544d55968c2d954
wisdom08 <wisdom08@kakao.com>
2025-04-30 00:58:19 +0900

    Add solution for Multiplesof3or5

---
+++ codewars/Multiplesof3or5.py
@@ -0,0 +1,14 @@
+# https://www.codewars.com/kata/514b92a657cdc65150000006/train/python
+# 회고: 주어진 숫자보다 작다는 거 놓침. 조건을 잘 확인하자
+
+def solution(number):
+    if number < 0:
+        return 0
+
+    res = set()
+
+    for i in range(3, number):
+        if i % 3 == 0 or i % 5 == 0:
+            res.add(i)
+
+    return sum(res)
